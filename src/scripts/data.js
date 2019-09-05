4b487172090d8d3c8d41d14f04c774cfea8b1c6f
Taylor Carroll <taylorcarroll@Taylors-MacBook-Pro.local>
2019-09-05 15:43:38 -0500

    saving

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -6,12 +6,65 @@ const API = {
     }
 } */
 
-/* This is Gradi's fetch call draft  
-let getMeetUps = () =>
-{fetch(`https://www.eventbriteapi.com/v3/events/search/?q=sports&location.address=nashville&token=LCTRAOJH5AJUT32CZU2C`), {
-  "headers": {
-      "Accept": "application/json"
-  }
-}.then(response => response.json())
-.then(parsedResponse => console.log(parsedResponse))
-} */
+
+// const apiParks = {
+//     getParks: function() {
+//         return fetch("https://data.nashville.gov/resource/74d7-b74t.json")
+//         .then(response => response.json())
+//         .then(parsedResponse => {
+//             //console.table makes the information look way better//
+//             console.table(parsedResponse)
+//     })} 
+
+
+    // fetch("https://data.nashville.gov/resource/74d7-b74t.json")
+    // .then(response => response.json())
+    // .then(parsedResponse => {
+    //     //console.table makes the information look way better//
+    //     console.table(parsedResponse)
+
+//     //function to push input to database.json//
+// const apiParks = {
+//     getParks: function() {
+//         return fetch("https://data.nashville.gov/resource/74d7-b74t.json")
+//         .then(response => 
+//             response.json()
+//         );
+//     },
+//     saveJournalEntry: function(entry) {
+//         return fetch("http://localhost:8088/journalArray", {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json"
+//             },
+//             body: JSON.stringify(entry)
+//         });
+//     }
+// };
+
+
+
+// const apiParks = {
+//     getParks: function() {
+//         return fetch("https://data.nashville.gov/resource/74d7-b74t.json")
+//         .then(response => 
+//             response.json()
+//         );
+//     }};
+
+
+//  //Events Fetch Call 
+//  fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey=qOiGAYZwGThoAEA10iytRGEa3c4RPX6K")
+//  .then(foods => foods.json())
+//  .then(parsedFoods => {
+//      console.log(parsedFoods)
+//  })
+
+
+ const parkFetcher = () => 
+     fetch(`https://data.nashville.gov/resource/74d7-b74t.json?$$app_token=i4q8JB7V9yqEY0Pt9c3JmDswS`)
+        .then(response => response.json())
+        .then(parsedResponse => 
+            console.log(parsedResponse)
+        )
+        

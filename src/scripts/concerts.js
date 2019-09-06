0fa436dfcaf21af6a97208fbf031c85f4e4703cf
David Cornish <davidcornish@Davids-MacBook-Pro.local>
2019-09-06 13:19:07 -0500

    cleaned up JS code

--- src/scripts/concerts.js
+++ src/scripts/concerts.js
@@ -1,11 +1,6 @@
-console.log("this is working")
-
- //looks for the first HTML element that has a class of mainContainer and stores it as a variable, this links to DOM
-//  const concertContainer = document.querySelector(".resultsContainer")
-//  console.log(concertContainer)
-
 
 //This is the web component 
+
 let concertComponent = (concerts) => {
     console.log("concertComponent function is called")
     return `
@@ -17,23 +12,23 @@ let concertComponent = (concerts) => {
         </div>`
     }
 
-console.log(concertComponent)
-
 //passes what it is given to the DOM
+
 let addToConcertDom = (htmlString) => {
     documentContainer.innerHTML += htmlString; 
     } 
 
-//this is a fetch call to bring in the parks in Nashville//
+
+// event listener which combines with dropdown html to bring results for each selection
+
 document.querySelector(".concertsButton").addEventListener("click", function () {
 concertType = document.querySelector(".searchByGenre").value
 concertGrab(concertType);
 
 })
 
-//add in code to look for the variable in the dropdown 
-//<option value="overall">How are you Feeling?</option>
-//
+
+//this is a fetch call to bring in the concerts in Nashville//
 
 function concertGrab(genre) {
      fetch(`https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=343&apikey=qOiGAYZwGThoAEA10iytRGEa3c4RPX6K&classificationName=${genre}`)
